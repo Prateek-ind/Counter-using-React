c616f098890bec5912991fd86ada6e02e01f1e21
Prateek <prateek.kumar.ind@gmail.com>
2025-06-19 12:06:41 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Counter-using-React/",
+  base: "/Counter-using-React",
   plugins: [react(), tailwindcss()],
 });
