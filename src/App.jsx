753a46c072139b6f4cc2b27b18a7c3adcedbf1d6
Prateek <prateek.kumar.ind@gmail.com>
2025-07-08 13:30:53 +0530

    Refactored using useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,67 @@
-import { useState } from "react";
+import { useReducer, useRef } from "react";
 import Button from "./Button";
 
+const initialState = { count: 0, inputNum: 1 };
+
+function counterReducer(state, action) {
+  const updatedState = { ...state };
+  if (action.type === "DECREMENT") {
+    updatedState.count -= action.payload;
+    return updatedState;
+  }
+  if (action.type === "INCREMENT") {
+    updatedState.count += action.payload;
+    return updatedState;
+  }
+  if (action.type === "RESET") {
+    updatedState.count = 0;
+    return updatedState;
+  }
+  return state;
+}
+
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [inputNum, setInputNum] = useState(1);
-  function decrease() {
-    setCount((prevCount) => prevCount - inputNum);
+  const [countState, countStateDispatch] = useReducer(
+    counterReducer,
+    initialState
+  );
+  const inputOperator = useRef();
+
+  function handleDecrement() {
+    countStateDispatch({
+      type: "DECREMENT",
+      payload: initialState.inputNum,
+    });
   }
-  function increase() {
-    setCount((prevCount) => prevCount + inputNum);
+  function handleIncrement() {
+    countStateDispatch({
+      type: "INCREMENT",
+      payload: initialState.inputNum,
+    });
   }
-  function reset() {
-    setCount(0);
+  function handleReset() {
+    countStateDispatch({
+      type: "RESET",
+    });
   }
 
   function handleInput(e) {
     const val = e.target.value;
 
     if (val === "") {
-      setInputNum(""); // Let it be empty
+      countState.inputNum = ""; // Let it be empty
     } else {
       const cleaned = val.replace(/^0+(?!$)/, "");
-      setInputNum(Number(cleaned));
+      countState.inputNum = Number(cleaned);
     }
   }
 
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
-      <h1 className="text-6xl font-bold mb-4">{count}</h1>
+      <h1 className="text-6xl font-bold mb-4">{countState.count}</h1>
       <div>
-        <Button clickHandler={decrease} value="-" />
-        <Button clickHandler={increase} value="+" />
+        <Button clickHandler={handleDecrement} value="-" />
+        <Button value="+" clickHandler={handleIncrement} />
       </div>
       <div className="mt-4 mb-4 flex flex-row gap-2">
         <p>Increment/Decrement by</p>
@@ -39,11 +70,12 @@ const App = () => {
           type="text"
           inputMode="numeric"
           pattern="[0-9]*"
-          value={inputNum}
+          value={countState.inputNum}
+          ref={inputOperator}
           onChange={handleInput}
         />
       </div>
-      <Button clickHandler={reset} value="reset" />
+      <Button value="reset" clickHandler={handleReset} />
     </main>
   );
 };
